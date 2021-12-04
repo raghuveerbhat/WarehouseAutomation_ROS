bf9353192ca9f03676867e3e80cc0629c92d5f85
vineeth2309 <vineeth2309@gmail.com>
2021-12-04 16:01:32 +0000

    Modified rates

--- src/pf_localisation-master/src/pf_localisation/pf_base.py
+++ src/pf_localisation-master/src/pf_localisation/pf_base.py
@@ -107,7 +107,7 @@ class PFLocaliserBase(object):
             self.update_particle_cloud(scan)
             self.particlecloud.header.frame_id = "map"
             self.estimatedpose.pose.pose = self.estimate_pose()
-            currentTime = rospy.Time.now() + rospy.Duration(1)
+            currentTime = rospy.Time.now() + rospy.Duration(0.5)
 
             # ----- Given new estimated pose, now work out the new transform
             self.recalculate_transform(currentTime)
@@ -280,4 +280,3 @@ class PFLocaliserBase(object):
         rospy.loginfo("Particle filter got map. (Re)initialising.")
         self.particlecloud = self.initialise_particle_cloud(self.estimatedpose)
         self.particlecloud.header.frame_id = "map"
-
